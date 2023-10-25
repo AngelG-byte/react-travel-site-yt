ea89bdb02b8b281a2b6096ded2f2e683193165f2
angelmarte <robotking134@gmail.com>
2023-10-25 19:50:06 -0400

    the start of something beautiful 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-
+import React  from "react";
 import './App.css';
+import Navbar from "./components/Navbar";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
+  const AppLayout = () => (
+  <>
+  <Navbar/>
+  <Outlet/>
+  </>);
+  const router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path="/" element={<AppLayout/>}>
 
-    </div>
+        </Route>
+      ))
+  return (
+    <>
+    <RouterProvider router={router}/>
+
+</>
   );
-}
+  }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+import {Link} from 'react';
+
+function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+            <h1>YOO</h1>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
